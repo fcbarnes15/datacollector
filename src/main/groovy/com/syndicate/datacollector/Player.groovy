bf0003995d281a6ee9be908bfe4f1d48a59ed6fa
fbarnes <fbarnes@lexmark.com>
2015-10-13 23:11:11 -0400

    initial seeding of application

---
+++ src/main/groovy/com/syndicate/datacollector/Player.groovy
@@ -0,0 +1,17 @@
+package com.syndicate.datacollector
+
+class Player {
+
+    long id
+    String name
+    boolean onCurrentRoster
+    String fromYear
+    String toYear
+    String playerCode
+    long teamId
+    String teamCity
+    String teamName
+    String teamAbbreviation
+    String teamCode
+    String gamesPlayedFlag
+}
